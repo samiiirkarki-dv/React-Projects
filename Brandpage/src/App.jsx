1316c943a446f722d77a1ee721c4e6cd6be1e7bc
samir karki <samirkarki123@gmail.com>
2026-04-10 01:00:11 +0545

    Split navbar code into seperate files

--- Brandpage/src/App.jsx
+++ Brandpage/src/App.jsx
@@ -1,22 +1,9 @@
-import './App.css'
-
+import './App.css';
+import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo" />
-        </div>
-
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Loaction</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        
-        <button>login</button>
-      </nav>
+      <Navigation />
     </div>
   );
 };
