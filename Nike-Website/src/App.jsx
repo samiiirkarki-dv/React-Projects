92a5fb4d08a584dc4956065fe73c373daabfcb59
samir karki <samirkarki123@gmail.com>
2026-04-24 22:27:22 +0545

    Create all the working files and setup

--- Nike-Website/src/App.jsx
+++ Nike-Website/src/App.jsx
@@ -1,7 +1,50 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
+const App = () => (
+  <main className="relative">
+    {/* <Nav /> */}
+
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+
+    <section className="padding">
+      <PoppularProducts />
+    </section>
+
+    <section className="padding">
+      <SuperQuality />
+    </section>
+
+    <section className="padding">
+      <Services />
+    </section>
+
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+
+    <section className="bg-green-600 padding">
+      <CustomerReviews />
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
