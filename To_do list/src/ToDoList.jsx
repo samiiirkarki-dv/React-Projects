1b62119e1ba8f474e9bf724a843216208aa42dc0
samir karki <samirkarki123@gmail.com>
2026-05-20 01:09:06 +0545

    create files

---
+++ To_do list/src/ToDoList.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function ToDoList(){
+
+    const [tasks, setTasks] = useState([]);
+    const [newTasks, setNewTasks]= useState("");
+
+    function handleInputChange(){
+    
+    }
+
+    return(
+        <></>
+    );
+}
+
+export default ToDoList;
